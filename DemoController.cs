34ca4c9af48165aeca2705a1e76d3e001f2fd115
Rameshwar91 <32979101+Rameshwar91@users.noreply.github.com>
2017-10-21 15:51:46 +0530

    commit

---
+++ DemoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DemoApplication.BL;
+using DemoApplication.Models;
+
+namespace DemoApplication.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(DemoModel demo)
+        {
+            DemoBL b = new DemoBL();
+            List<DemoModel> a = b.DemoBuisness(demo);
+            return View("~/Views/Demo/Index.cshtml", a);
+        }
+    }
+}
